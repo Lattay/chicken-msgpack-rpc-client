931e85f9b412c01dc008d41bc62380339f538b60
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2020-01-06 11:36:10 +0100

    Prevent misinterpretation of empty parameter list as null

--- src/mrpc-protocol.scm
+++ src/mrpc-protocol.scm
@@ -41,11 +41,11 @@
   (define (write-request port id method-name params)
     (assert (and 'write-request (integer? id)))
     (assert (and 'write-request (output-port? port) (output-port-open? port)))
-    (mpk:pack port (list 0 id method-name params)))
+    (mpk:pack port (list 0 id method-name (if (null? params) #() params))))
 
   (define (write-notification port method-name params)
     (assert (and 'write-notification (output-port? port) (output-port-open? port)))
-    (mpk:pack port (list 2 method-name params)))
+    (mpk:pack port (list 2 method-name (if (null? params) #() params))))
 
   (define (write-response port id method-name error-obj result)
     (assert (and 'write-notification (integer? id)))
